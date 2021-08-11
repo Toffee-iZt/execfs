8de4f092c37320ba0ab9ea02c7a0e8da9f09f693
Toffee-iZt <mgsiask@gmail.com>
2021-08-11 17:15:45 +0300

    fix temp

--- temp.go
+++ temp.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"sync"
 	"time"
 )
 
@@ -14,50 +15,46 @@ func CreateTemp(suffix string) (*os.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	return os.OpenFile(tempabs(path), os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+	return os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 }
 
 // CreateTempDir creates temp directory.
-func createTempDir(suffix string) (string, error) {
+func CreateTempDir(suffix string) (string, error) {
 	path, err := tempPath(suffix)
 	if err != nil {
 		return "", err
 	}
-	return path, os.Mkdir(tempabs(path), 0700)
+	return path, os.Mkdir(path, 0700)
 }
 
-var tempdir, worktemp string
-
-func init() {
-	tempdir = os.TempDir()
-	var err error
-	worktemp, err = createTempDir(GetExecName())
-	if err != nil {
-		panic(err)
-	}
-}
+var tempdir = os.TempDir()
+var worktemp string
+var wconce sync.Once
 
 // GetTempDir returns the system temp directory.
 func GetTempDir() string {
-	return dir
+	return tempdir
 }
 
 // GetWorkTemp returns the working temp directory.
 func GetWorkTemp() string {
-	return name
-}
-
-func tempabs(name string) string {
-	return filepath.Join(worktemp, name)
+	return worktemp
 }
 
 var errHasSeparator = errors.New("suffix contains path separator")
 
 func tempPath(suffix string) (string, error) {
+	wconce.Do(func() {
+		worktemp = filepath.Join(tempdir, GetExecName())
+		err := os.Mkdir(worktemp, 0700)
+		if err != nil {
+			panic(err)
+		}
+	})
 	for i := 0; i < len(suffix); i++ {
 		if os.IsPathSeparator(suffix[i]) {
 			return "", &os.PathError{Op: "createtemp", Path: suffix, Err: errHasSeparator}
 		}
 	}
-	return tempabs(strconv.FormatInt(time.Now().UnixNano(), 10) + suffix), nil
+	return filepath.Join(worktemp, strconv.FormatInt(time.Now().UnixNano(), 10)+suffix), nil
 }
